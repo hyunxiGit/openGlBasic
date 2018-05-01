2248e9332f22cb5b2370112a7b8271ace6598f7d
hyunxi <hyunxi@hotmail.com>
2018-04-30 21:58:59 -0700

    1. add more cpp to the cpp project which can correctly reference 2. add Vector class - todo : impliment vector class functions

---
+++ openGlBasicLearning/COVector.cpp
@@ -0,0 +1,15 @@
+#include "stdafx.h"
+#include "COVector.h"
+using namespace std;
+void coTestFunction()
+{
+	printf("this is coTestFunction");
+}
+
+COVector2::COVector2(float _x, float _y)
+{
+	this->x = _x;
+	this->y = -y;
+	cout << "this->x  ";
+	cout << this->x << endl;
+}
---
+++ openGlBasicLearning/COVector.h
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef COVECTOR_H
+#define COVECTOR_H
+
+class COVector2
+{
+public:
+	float x;
+	float y;
+	COVector2(float, float);
+};
+void coTestFunction();
+
+#endif
